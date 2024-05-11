62197e4fbc69aafa576e6775d4b3417d0776a41b
naslyazghi <nas.lyazghi@knights.ucf.edu>
2024-05-11 19:05:50 -0400

    Placed the examples in the example folder

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-examples/examples"
+)
 
 // To run the program, put the code in hello-world.go and use go run.
 // Sometimes we’ll want to build our programs into binaries. We can do this using go build.
@@ -11,20 +14,20 @@ func main() {
 	fmt.Println(" 		G O L A N G  E X A M P L E S")
 	fmt.Println("----------------------------------------------------------")
 
-	// 2 - Values
-	Values()
+	// 2 - VALUES
+	examples.Values()
 	// 3 - Variables
-	Variables()
-	// 4 - Constants
-	Constants()
-	// 5 - For
-	For()
-	// 6 - If Else
-	IfElse()
-	// 7 - Switch
-	Switch()
-	// 8 - Arrays
-	Arrays()
-	// 9 - Slices
-	Slices()
+	examples.Variables()
+	// 4 - CONSTANTS
+	examples.Constants()
+	// 5 - FOR
+	examples.For()
+	// 6 - IF ELSE
+	examples.IfElse()
+	// 7 - SWITCH
+	examples.Switch()
+	// 8 - ARRAYS
+	examples.Arrays()
+	// 9 - SLICES
+	examples.Slices()
 }
